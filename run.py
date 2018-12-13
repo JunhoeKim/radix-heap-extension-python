0df3f760d9cf0171b39a8d003b8d9994a29b2e44
unknown <ytaek.kim@hcil.snu.ac.kr>
2018-12-13 16:18:42 +0900

    adjust input file node index (--)

--- run.py
+++ run.py
@@ -5,18 +5,22 @@ nodeDic = {}
 
 # Sample data used on http://www.dis.uniroma1.it/challenge9/download.shtml
 # input file format also follows origin file format.
+# ex) a 1 2 803
 for line in open(sys.argv[1]):
     line = line.strip()
     if line.startswith("a"):
         command, parentNode, childNode, distance = line.split()
         
-        parentNode = int(parentNode)
-        childNode = int(childNode)
+        parentNode = int(parentNode) - 1
+        childNode = int(childNode) - 1
         distance = int(distance)
+
+        # save data in dictionary
         nodeDic.setdefault(parentNode, []).append((childNode, distance))
     else:
         continue
 
+# convert dictionary to graph list
 inputGraphList = []
 for key in range(max(nodeDic.keys())+1):
     inputGraphList.append(nodeDic.setdefault(key, []))    
