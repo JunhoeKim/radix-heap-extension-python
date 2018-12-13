577ccdbcc718eb5db94e5cbcb94bb852a9af76c4
unknown <ytaek.kim@hcil.snu.ac.kr>
2018-12-13 17:20:37 +0900

    add node 20 sample data

---
+++ sample_geneartor.py
@@ -0,0 +1,17 @@
+import os, sys
+import random
+
+nodeCount = 20
+arcCountPerNode = 10
+maxDistance = 20
+
+for i in range(nodeCount):
+    # for j in range(random.randint(arcCountPerNode, arcCountPerNode)):
+    for j in range(i + 1, nodeCount):
+        dist = random.randint(1, maxDistance) + maxDistance - i
+        # dic[str(i) + " " + str(j)] = dist
+        # dic[str(j) + " " + str(i))] = dist
+        print("a", i+1, j+1, dist)
+        print("a", j+1, i+1, dist)
+        # print("a", i, random.randint(0, nodeCount), random.randint(1, maxDistance+1))
+
